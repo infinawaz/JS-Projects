06ceaa26141086928c59a13bd346dbbe251d9b78
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-03-07 12:50:13 +0530

    done

---
+++ Random Color Generator/js/app.js
@@ -0,0 +1,19 @@
+// const getColor=()=>{
+//     //creating random hex code
+//     const randomNumber = Math.floor(Math.random()*16777215);
+
+//     const randomCode = "#"+randomNumber.toString(16);
+//     console.log(randomNumber,randomCode);
+// }
+
+// document.getElementById("btn").addEventListener("click",getColor)
+
+//We can also write like this
+
+document.getElementById("btn").addEventListener("click",()=>{
+    //creating random hex code
+    const randomNumber = Math.floor(Math.random()*16777215);
+    const randomCode = "#"+randomNumber.toString(16);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("color-code").innerText=randomCode;
+})
