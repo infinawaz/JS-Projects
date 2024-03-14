b9a7fbe9d9c6ea24753829650e04002cd6dfdde5
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-03-14 14:18:30 +0530

    changes in app.js

--- Random Color Generator/js/app.js
+++ Random Color Generator/js/app.js
@@ -10,7 +10,7 @@
 
 //We can also write like this
 
-document.getElementById("btn").addEventListener("click",()=>{
+document.getElementById("btn").addEventListener("click",()=>{   //using arrow fn as a parameter  
     //creating random hex code
     const randomNumber = Math.floor(Math.random()*16777215);
     const randomCode = "#"+randomNumber.toString(16);
